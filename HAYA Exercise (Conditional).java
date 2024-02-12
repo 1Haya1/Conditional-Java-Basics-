40d9e6f83594a8224444899089f43cfce7f69eb0
1Haya1 <154345101+1Haya1@users.noreply.github.com>
2024-02-12 21:20:44 +0300

    Add files via upload

---
+++ HAYA Exercise (Conditional).java
@@ -0,0 +1,138 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+//EX1
+
+//        Scanner s = new Scanner(System.in);
+//
+//        System.out.print("Enter your role: ");
+//        String role = s.nextLine();
+//
+//        if (role.equalsIgnoreCase("admin")) {
+//            System.out.println("Welcome admin");
+//        } else if (role.equalsIgnoreCase("superuser")) {
+//            System.out.println("Welcome superuser");
+//        } else if (role.equalsIgnoreCase("user")) {
+//            System.out.println("Welcome user");
+//        } else {
+//            System.out.println("Unknown role. Please contact the administrator.");
+//        }
+
+//EX2
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Input the 1st number: ");
+//        int firstNumber = scanner.nextInt();
+//
+//        System.out.print("Input the 2nd number: ");
+//        int secondNumber = scanner.nextInt();
+//
+//        System.out.print("Input the 3rd number: ");
+//        int thirdNumber = scanner.nextInt();
+//
+//        int greatestNumber = findGreatestNumber(firstNumber, secondNumber, thirdNumber);
+//
+//        System.out.println("The greatest: " + greatestNumber);
+//    }
+//
+//    public static int findGreatestNumber(int num1, int num2, int num3) {
+//
+//        return Math.max(Math.max(num1, num2), num3);
+
+
+        //EX3
+
+//        Random random = new Random();
+//        int number = random.nextInt(7) + 1;
+//
+//        String weekdayName = getWeekdayName(number);
+//        System.out.println("The weekday is: " + weekdayName);
+//    }
+//
+//    public static String getWeekdayName(int number) {
+//        String weekdayName = "";
+//        switch (number) {
+//            case 1:
+//                weekdayName = "Sunday";
+//                break;
+//            case 2:
+//                weekdayName = "Monday";
+//                break;
+//            case 3:
+//                weekdayName = "Tuesday";
+//                break;
+//            case 4:
+//                weekdayName = "Wednesday";
+//                break;
+//            case 5:
+//                weekdayName = "Thursday";
+//                break;
+//            case 6:
+//                weekdayName = "Friday";
+//                break;
+//            case 7:
+//                weekdayName = "Saturday";
+//                break;
+//            default:
+//                System.out.println("Invalid number!");
+//        }
+//        return weekdayName;
+
+//        //EX4
+//        Scanner s = new Scanner(System.in);
+//
+//        System.out.print("Enter your numeric score: ");
+//        int score = s.nextInt();
+//
+//        String grade = convertToLetterGrade(score);
+//
+//        System.out.println("Numeric Score: " + score);
+//        System.out.println("Letter Grade: " + grade);
+//
+//    }
+//    public static String convertToLetterGrade(int score) {
+//        if (score >= 90 && score <= 100) {
+//            return "A";
+//        } else if (score >= 80 && score <= 89) {
+//            return "B";
+//        } else if (score >= 70 && score <= 79) {
+//            return "C";
+//        } else if (score >= 60 && score <= 69) {
+//            return "D";
+//        } else {
+//            return "F";
+
+
+
+        //EX5
+
+//        Scanner s = new Scanner(System.in);
+//
+//        System.out.print("Enter your age: ");
+//        int age = s.nextInt();
+//
+//        String category = categorizeAge(age);
+//
+//        System.out.println("You are a " + category + ".");
+//
+//    }
+//
+//    public static String categorizeAge(int age) {
+//        if (age < 13) {
+//            return "Child";
+//        } else if (age >= 13 && age <= 19) {
+//            return "Teenager";
+//        } else {
+//            return "Adult";
+//        }
+
+
+
+
+
+
+    }}
---
+++ HAYA Exercises (Java Basics).java
@@ -0,0 +1,155 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
+public class Main {
+            public static void main(String[] args) {
+
+//                 EX1
+//                Scanner s = new Scanner(System.in);
+//
+//                System.out.print("Enter weight in kilograms: ");
+//                double weight = s.nextDouble();
+//
+//                System.out.print("Enter height in meters: ");
+//                double height = s.nextDouble();
+//
+//
+//                System.out.println("BMI = " + weight / (height * height));
+
+
+                //EX2
+//                Scanner s = new Scanner(System.in);
+//
+//                System.out.print("Enter obtained Marks: ");
+//                double ObtainedMarks = s.nextDouble();
+//
+//                System.out.print("Enter Total marks ");
+//                double Totalmarks = s.nextDouble();
+//
+//double precentage= (ObtainedMarks / Totalmarks)*100;
+//                System.out.println("Precentage=" +precentage+"%" );
+
+// EX3
+//                Scanner s = new Scanner(System.in);
+//                System.out.print("Enter amount in USD: ");
+//                double amountUSD = s.nextDouble();
+//
+//
+//                System.out.print("Enter exchange rate (USD to EUR): ");
+//                double exchangeRate = s.nextDouble();
+//                double amountEUR = amountUSD * exchangeRate;
+//                System.out.println("Amount in EUR = " + amountEUR);
+
+//EX4
+//                Scanner scanner = new Scanner(System.in);
+//                System.out.print("Enter a string: ");
+//                String inputString = scanner.nextLine();
+//
+//                int length = inputString.length();
+//                System.out.println("Length of the string: " + length);
+//                StringBuilder reversedString = new StringBuilder(inputString);
+//                reversedString.reverse();
+//                System.out.println("Reversed string: " + reversedString);
+
+//EX5
+//                Scanner s = new Scanner(System.in);
+//                System.out.print("Enter a sentence: ");
+//                String sentence = s.nextLine();
+//
+//                System.out.print("Enter the start index: ");
+//                int startIndex = s.nextInt();
+//                s.nextLine();
+//
+//                System.out.print("Enter the end index: ");
+//                int endIndex = s.nextInt();
+//                s.nextLine();
+
+//                String substring = extractSubstring(sentence, startIndex, endIndex);
+//                System.out.println("Extracted substring: \"" + substring + "\"");
+//
+//
+//            }
+//
+//    public static String extractSubstring(String sentence, int startIndex, int endIndex) {
+//        if (startIndex < 0) {
+//            startIndex = 0;
+//        }
+//        if (endIndex >= sentence.length()) {
+//            endIndex = sentence.length() - 1;
+//        }
+//        return sentence.substring(startIndex, endIndex + 1);
+
+                //EX6
+//                Scanner s = new Scanner(System.in);
+//                System.out.print("Enter a sentence: ");
+//                String sentence = s.nextLine();
+//
+//                System.out.print("Enter a keyword: ");
+//                String keyword = s.nextLine();
+//
+//                boolean isPresent = checkKeywordPresence(sentence, keyword);
+//
+//                if (isPresent) {
+//                    System.out.println("Keyword \"" + keyword + "\" is present in the sentence.");
+//                } else {
+//                    System.out.println("Keyword \"" + keyword + "\" is not present in the sentence.");
+//                }
+//            }
+//
+//    public static boolean checkKeywordPresence(String sentence, String keyword) {
+//        // تحويل الجملة والكلمة المفتاحية إلى حروف صغيرة للمقارنة بدون حساسية لحالة الأحرف
+//        sentence = sentence.toLowerCase();
+//        keyword = keyword.toLowerCase();
+//
+//        return sentence.contains(keyword);
+
+//EX7
+
+//                Scanner s = new Scanner(System.in);
+//
+//                System.out.print("Enter a sentence: ");
+//                String sentence = s.nextLine();
+//
+//                System.out.print("Enter the word to replace: ");
+//                String wordToReplace = s.next();
+//
+//                System.out.print("Enter the replacement word: ");
+//                String replacementWord = s.next();
+//
+//                String modifiedSentence = replaceWord(sentence, wordToReplace, replacementWord);
+//
+//                System.out.println("Modified Sentence: " + modifiedSentence);
+//            }
+//                public static String replaceWord(String sentence, String wordToReplace, String replacementWord) {
+//
+//                    return sentence.replaceAll(wordToReplace, replacementWord);
+
+      //  EX8
+
+//                Scanner s = new Scanner(System.in);
+//
+//                System.out.print("Enter String 1: ");
+//                String string1 = s.nextLine();
+//
+//                System.out.print("Enter String 2: ");
+//                String string2 = s.nextLine();
+//
+//                boolean areEqual = compareStringsIgnoreCase(string1, string2);
+//
+//                if (areEqual) {
+//                    System.out.println("Strings are equal");
+//                } else {
+//                    System.out.println("Strings are not equal");
+//                }
+//
+//            }
+//    public static boolean compareStringsIgnoreCase(String str1, String str2) {
+//
+//        return str1.equalsIgnoreCase(str2);
+
+
+
+        }}
+
